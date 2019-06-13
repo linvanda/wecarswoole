918a981b3eb53bf449f6cf2bc6fa877ae57b76f9
linvanda <1049180458@qq.com>
2019-06-13 15:43:23 +0800

    增加phpstorm提示

---
+++ template/.phpstorm.meta.php
@@ -0,0 +1,15 @@
+<?php
+namespace PHPSTORM_META
+{
+    $STATIC_METHOD_TYPES = [
+        \Psr\Container\ContainerInterface::get('') => [
+            "" == "@",
+        ],
+        \DI\Container::get('') => [
+            "" == "@",
+        ],
+        \WecarSwoole\Container::get('') => [
+            "" == "@",
+        ]
+    ];
+}
