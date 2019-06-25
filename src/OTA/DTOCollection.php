5dbeb9477e174703cd5980da9097a8ae3a951a2a
linvanda <1049180458@qq.com>
2019-06-25 11:13:20 +0800

    opt

--- src/OTA/DTOCollection.php
+++ src/OTA/DTOCollection.php
@@ -10,25 +10,41 @@ use WecarSwoole\DTO;
  */
 class DTOCollection extends Collection
 {
+    private $class;
+
     /**
      * 通过传入二维数组自动创建 DTO 对象
      * DTOCollection constructor.
-     * @param string $classType DTO 具体的类型
+     * @param string $class DTO 具体的类型
      * @param array $array 可构建 DTO 对象的二维数组
      * @throws \Exception
      */
-    public function __construct(string $classType, array $array = [])
+    public function __construct(string $class, array $array = [])
     {
-        $class = new \ReflectionClass($classType);
-        if (!$class->isSubclassOf(DTO::class)) {
-            throw new \Exception("$classType is not sub class of DTO");
+        $this->class = $class;
+        $cls = new \ReflectionClass($class);
+        if (!$cls->isSubclassOf(DTO::class)) {
+            throw new \Exception("$class is not sub class of DTO");
         }
 
         $objArr = [];
         foreach ($array as $row) {
-            $objArr[] = $class->newInstance($row);
+            $objArr[] = $cls->newInstance($row);
         }
 
         parent::__construct($objArr);
+    }
+
+    /**
+     * @param mixed $value
+     * @throws \Exception
+     */
+    public function append($value)
+    {
+        if (is_array($value)) {
+            $value = (new \ReflectionClass($this->class))->newInstance($value);
+        }
+
+        parent::append($value);
     }
 }
--- template/config/di/di.php
+++ template/config/di/di.php
@@ -6,15 +6,17 @@ use Psr\SimpleCache\CacheInterface;
 use Psr\Log\LoggerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 
+use function DI\{create, factory};
+
 return [
     // 仓储
-    'App\Domain\*\I*Repository' => \DI\create('\App\Foundation\Repository\*\MySQL*Repository'),
+    'App\Domain\*\I*Repository' => create('\App\Foundation\Repository\*\MySQL*Repository'),
     // 缓存
-    CacheInterface::class => \DI\factory([CacheFactory::class, 'build']),
+    CacheInterface::class => factory([CacheFactory::class, 'build']),
     // 日志
-    LoggerInterface::class => \DI\factory(function () {
+    LoggerInterface::class => factory(function () {
         return Logger::getInstance();
     }),
     // 事件
-    EventDispatcherInterface::class => \DI\create(\Symfony\Component\EventDispatcher\EventDispatcher::class),
+    EventDispatcherInterface::class => create(\Symfony\Component\EventDispatcher\EventDispatcher::class),
 ];
