4b2d0230b6ba91cdab9878e27b62e7d91fdb1686
linvanda <1049180458@qq.com>
2019-08-22 10:21:52 +0800

    opt

--- template/config/apollo.php
+++ template/config/apollo.php
@@ -1,14 +1,15 @@
 <?php
+
 /**
  *  Client 配置中心
  */
 return [
     'app_id' => 1000090,
     'server' => [
-        'dev' => 'develop.configserver.zhyz.cn:8080',
-        'test' => 'test.configserver.zhyz.cn:8080',
-        'preview' => 'preview.configserver.zhihuiyouzhan.cn:8080',
-        'produce' => 'production.configserver.zhihuiyouzhan.cn:8080',
+        'dev' => 'http://develop.configserver.zhyz.cn:8080',
+        'test' => 'http://test.configserver.zhyz.cn:8080',
+        'preview' => 'http://preview.configserver.zhihuiyouzhan.cn:8080',
+        'produce' => 'http://production.configserver.zhihuiyouzhan.cn:8080',
     ],
     // 需要监听的 namespace
     'namespaces' => [
